a4f0bf65d9005e6df53ba88a7fa2ba8c361d95c2
otanadzetsotne <otanadzetsotne@yahoo.com>
2023-11-10 22:23:18 +0400

    html to pdf | css fixed

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cv_generator
 This application helps generate exclusive CV from specific vacancy.
+
+### Set up project
+
+* Install **wkhtmltopdf** for html to pdf converting
+    ```bash
+    brew install --cask wkhtmltopdf
+    ```
--- gen.py
+++ gen.py
@@ -1,4 +1,5 @@
 import json
+import subprocess
 from pathlib import Path
 
 from generators.base import BaseGenerator
@@ -6,14 +7,18 @@ from generators.base import BaseGenerator
 
 root_dir = Path(__file__).parent
 cv_path = root_dir / 'test.cv.json'
-html_path = root_dir / 'templates' / 'html' / 'base'
+templates_path = root_dir / 'templates' / 'html' / 'base'
 
 with open(cv_path, 'r') as f:
     cv = json.load(f)
 
 
-generator = BaseGenerator(templates_path=html_path, cv=cv)
+generator = BaseGenerator(templates_path=templates_path, cv=cv)
 html = generator.full()
 
-with open(root_dir / 'test.html', 'w') as f:
+html_path = root_dir / 'test.html'
+with open(html_path, 'w') as f:
     f.write(html)
+
+pdf_path = root_dir / 'test.pdf'
+subprocess.check_output(['wkhtmltopdf', f'file://{html_path}', pdf_path])
