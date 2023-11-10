5daf482d65fe7388ddb57baf552a84adb3ab6e4b
otanadzetsotne <otanadzetsotne@yahoo.com>
2023-11-10 22:39:33 +0400

    venv setup script

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# cv_generator
+# CV Generator
 This application helps generate exclusive CV from specific vacancy.
 
 ### Set up project
 
-* Install **wkhtmltopdf** for html to pdf converting
-    ```bash
-    brew install --cask wkhtmltopdf
-    ```
+Run environment setup script. This script creates **python virtual environment**
+in project root dir and installs all required python dependencies 
+```bash
+./setup.sh
+```
+
+Install **wkhtmltopdf** for html to pdf converting
+```bash
+brew install --cask wkhtmltopdf
+```
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set the ROOT_DIR environment variable to the script's current directory
+export ROOT_DIR=$(dirname "$(realpath "$0")")
+
+# Directory for the virtual environment, inside the ROOT_DIR
+VENV_DIR="$ROOT_DIR/venv"
+
+# Check if the virtual environment directory exists
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment in $ROOT_DIR..."
+    python -m venv "$VENV_DIR"
+fi
+
+# Activate the virtual environment
+source "$VENV_DIR/bin/activate"
+
+# Path to the requirements.txt file
+REQ_FILE="$ROOT_DIR/requirements.txt"
+
+# Check if requirements.txt exists and install any missing packages
+if [ -f "$REQ_FILE" ]; then
+    echo "Installing dependencies from $REQ_FILE..."
+    pip install -r "$REQ_FILE"
+else
+    echo "requirements.txt not found in $ROOT_DIR."
+fi
+
+echo "Setup complete."
